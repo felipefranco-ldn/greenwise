d3fb9562de3d61f5924037bc6d96392c7cec1f2a
Felipe Franco <felipefranco8610@gmail.com>
2021-12-05 14:26:23 +0000

    Setup Layout, PageHeader and PageFooter components

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import PageFooter from '../PageFooter/PageFooter';
+import PageHeader from '../PageHeader/PageHeader';
 import './Layout.scss';
 
-export default function Layout() {
-  return <div></div>;
+export default function Layout({ children }) {
+  return (
+    <>
+      <PageHeader />
+      <section className="layout">
+        <div className="layout__box">{children}</div>
+      </section>
+      <PageFooter />
+    </>
+  );
 }
