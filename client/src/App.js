1c068dc960c1fa793fba698630b59f7d5f0e212d
Felipe Franco <felipefranco8610@gmail.com>
2021-12-05 14:13:19 +0000

    Setup routes in App.js and link corresponding pages/components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Estimate from './pages/estimate';
+import Home from './pages/home';
+import Offset from './pages/offset';
+import Profile from './pages/profile';
+import Reduce from './pages/reduce';
+import './styles/App.scss';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="app">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/estimate" exact component={Estimate} />
+              <Route path="/reduce" exact component={Reduce} />
+              <Route path="/offset" exact component={Offset} />
+              <Route path="/profile" exact component={Profile} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
---
+++ client/src/pages/estimate/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Estimate extends Component {
+  render() {
+    return <div></div>;
+  }
+}
