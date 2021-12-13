fd84c9e55bd68a82522f7277f362aff13d0969d1
Felipe Franco <felipefranco8610@gmail.com>
2021-12-13 11:29:11 +0000

    Setup user authentication functionality using Firebase

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import Compensate from './pages/compensate';
 import Profile from './pages/profile';
 import Reduce from './pages/reduce';
 import './styles/App.scss';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Reset from './components/Reset/Reset';
+import Account from './components/Account/Account';
 
 export default class App extends Component {
   render() {
@@ -29,6 +33,10 @@ export default class App extends Component {
               <Route path="/reduce" component={Reduce} />
               <Route path="/compensate" component={Compensate} />
               <Route path="/my-account" component={Profile} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/reset" component={Reset} />
+              <Route exact path="/account" component={Account} />
             </Switch>
           </div>
         </BrowserRouter>
