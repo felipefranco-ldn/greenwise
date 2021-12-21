6f6fafced4860ecc0819ed3b4b6c852d2e2b8879
Felipe Franco <felipefranco8610@gmail.com>
2021-12-21 17:26:04 +0000

    Refactor FlightsInterface to reduce extent of component (FlightsInput and FlightsOutput components created)

---
+++ client/src/components/FlightsInterface/FlightsInput.js
@@ -0,0 +1,200 @@
+import React from 'react';
+
+export default function FlightsInput({ handleChange, handleClassChange }) {
+  return (
+    <div className="fl-input">
+      <form className="fl-input__form">
+        <div className="fl-input__form-question">
+          <label className="fl-input__form-label">
+            <span className="span">{'>>'} </span> How long was your flight?
+          </label>
+
+          <div className="fl-input__form-duration-box">
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="domestic"
+              name="flightDuration"
+              value="domestic"
+              onChange={handleChange}
+            />
+            <label className="fl-input__form-duration" htmlFor="domestic">
+              <h3 className="fl-input__form-label-title">Regional</h3>
+              <p className="fl-input__form-label-info">
+                Up to 1:40h or <br />
+                950km / 590mi
+              </p>
+              <p className="fl-input__form-label-example">
+                e.g. London - Berlin
+              </p>
+            </label>
+
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="short"
+              name="flightDuration"
+              value="short"
+              onChange={handleChange}
+            />
+            <label className="fl-input__form-duration" htmlFor="short">
+              <h3 className="fl-input__form-label-title">Short Haul</h3>
+              <p className="fl-input__form-label-info">
+                Up to 3:20h or 2200km / 1370mi
+              </p>
+              <p className="fl-input__form-label-example">e.g. London - Kiev</p>
+            </label>
+          </div>
+
+          <div className="fl-input__form-duration-box">
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="medium"
+              name="flightDuration"
+              value="medium"
+              onChange={handleChange}
+            />
+            <label className="fl-input__form-duration" htmlFor="medium">
+              <h3 className="fl-input__form-label-title">Medium Haul</h3>
+              <p className="fl-input__form-label-info">
+                Up to 6:40h or 5300km / 3300mi
+              </p>
+              <p className="fl-input__form-label-example">
+                e.g. London - Boston
+              </p>
+            </label>
+
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="long"
+              name="flightDuration"
+              value="long"
+              onChange={handleChange}
+            />
+            <label className="fl-input__form-duration" htmlFor="long">
+              <h3 className="fl-input__form-label-title">Long Haul</h3>
+              <p className="fl-input__form-label-info">
+                Up to 11:30h or 9600km / 5970mi
+              </p>
+              <p className="fl-input__form-label-example">
+                e.g. London - Bangkok
+              </p>
+            </label>
+          </div>
+
+          <div className="fl-input__form-duration-box">
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="extraLong"
+              name="flightDuration"
+              value="extraLong"
+              onChange={handleChange}
+            />
+            <label className="fl-input__form-duration" htmlFor="extraLong">
+              <h3 className="fl-input__form-label-title">Extra-long Haul</h3>
+              <p className="fl-input__form-label-info">
+                Up to 16:40h or 14500km / 9000mi
+              </p>
+              <p className="fl-input__form-label-example">
+                e.g. London - Perth
+              </p>
+            </label>
+
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="ultraLong"
+              name="flightDuration"
+              value="ultraLong"
+              onChange={handleChange}
+            />
+            <label className="fl-input__form-duration" htmlFor="ultraLong">
+              <h3 className="fl-input__form-label-title">Ulta-long Haul</h3>
+              <p className="fl-input__form-label-info">
+                Up to 24:00h or 18400km / 11370mi
+              </p>
+              <p className="fl-input__form-label-example">
+                e.g. London - Auckland
+              </p>
+            </label>
+          </div>
+        </div>
+
+        <div className="fl-input__break"></div>
+
+        <div className="fl-input__form-question">
+          <label className="fl-input__form-label">
+            <span className="span">{'>>'} </span> What class did you fly in?
+          </label>
+
+          <div className="fl-input__form-class-box">
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="economy"
+              name="flightClass"
+              value="Economy"
+              onChange={handleClassChange}
+            />
+            <label className="fl-input__form-class" htmlFor="economy">
+              <h3 className="fl-input__form-label-title-class">
+                🌱 Economy
+                <br /> Class
+              </h3>
+            </label>
+
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="premiumEconomy"
+              name="flightClass"
+              value="PremiumEconomy"
+              onChange={handleClassChange}
+            />
+            <label className="fl-input__form-class" htmlFor="premiumEconomy">
+              <h3 className="fl-input__form-label-title-class">
+                💸 Premium
+                <br /> Economy{' '}
+              </h3>
+            </label>
+          </div>
+
+          <div className="fl-input__form-class-box">
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="Business"
+              name="flightClass"
+              value="Business"
+              onChange={handleClassChange}
+            />
+            <label className="fl-input__form-class" htmlFor="Business">
+              <h3 className="fl-input__form-label-title-class">
+                💰 Business
+                <br /> Class
+              </h3>
+            </label>
+
+            <input
+              className="fl-input__form-radio"
+              type="radio"
+              id="first"
+              name="flightClass"
+              value="First"
+              onChange={handleClassChange}
+            />
+            <label className="fl-input__form-class" htmlFor="first">
+              <h3 className="fl-input__form-label-title-class">
+                🧐 First <br />
+                Class
+              </h3>
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- client/src/components/FlightsInterface/FlightsInterface.js
+++ client/src/components/FlightsInterface/FlightsInterface.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import { EXT_API_KEY, EXT_API_URL } from '../../utils/api';
 import './FlightsInterface.scss';
 import axios from 'axios';
-import flightsMap from '../../assets/images/images/flights-map.png';
-import { Link } from 'react-router-dom';
-import co2Logo from '../../assets/images/images/co2-logo.svg';
+import FlightsInput from './FlightsInput';
+import FlightsOutput from './FlightsOutput';
 
 import {
   Chart as ChartJS,
@@ -32,6 +31,7 @@ export default class FlightsInterface extends Component {
     userFlightClass: '',
     userFlightCo2: 0,
     totalFlightCo2: 0,
+    chartData: 0,
   };
 
   handleChange = (event) => {
@@ -253,277 +253,22 @@ export default class FlightsInterface extends Component {
     return (
       <div className="fl-container">
         {/* //! input section (left side) starts here */}
-        <div className="fl-input">
-          <form className="fl-input__form">
-            <div className="fl-input__form-question">
-              <label className="fl-input__form-label">
-                <span className="span">{'>>'} </span> How long was your flight?
-              </label>
 
-              <div className="fl-input__form-duration-box">
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="domestic"
-                  name="flightDuration"
-                  value="domestic"
-                  onChange={this.handleChange}
-                />
-                <label className="fl-input__form-duration" htmlFor="domestic">
-                  <h3 className="fl-input__form-label-title">Regional</h3>
-                  <p className="fl-input__form-label-info">
-                    Up to 1:40h or <br />
-                    950km / 590mi
-                  </p>
-                  <p className="fl-input__form-label-example">
-                    e.g. London - Berlin
-                  </p>
-                </label>
-
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="short"
-                  name="flightDuration"
-                  value="short"
-                  onChange={this.handleChange}
-                />
-                <label className="fl-input__form-duration" htmlFor="short">
-                  <h3 className="fl-input__form-label-title">Short Haul</h3>
-                  <p className="fl-input__form-label-info">
-                    Up to 3:20h or 2200km / 1370mi
-                  </p>
-                  <p className="fl-input__form-label-example">
-                    e.g. London - Kiev
-                  </p>
-                </label>
-              </div>
-
-              <div className="fl-input__form-duration-box">
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="medium"
-                  name="flightDuration"
-                  value="medium"
-                  onChange={this.handleChange}
-                />
-                <label className="fl-input__form-duration" htmlFor="medium">
-                  <h3 className="fl-input__form-label-title">Medium Haul</h3>
-                  <p className="fl-input__form-label-info">
-                    Up to 6:40h or 5300km / 3300mi
-                  </p>
-                  <p className="fl-input__form-label-example">
-                    e.g. London - Boston
-                  </p>
-                </label>
-
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="long"
-                  name="flightDuration"
-                  value="long"
-                  onChange={this.handleChange}
-                />
-                <label className="fl-input__form-duration" htmlFor="long">
-                  <h3 className="fl-input__form-label-title">Long Haul</h3>
-                  <p className="fl-input__form-label-info">
-                    Up to 11:30h or 9600km / 5970mi
-                  </p>
-                  <p className="fl-input__form-label-example">
-                    e.g. London - Bangkok
-                  </p>
-                </label>
-              </div>
-
-              <div className="fl-input__form-duration-box">
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="extraLong"
-                  name="flightDuration"
-                  value="extraLong"
-                  onChange={this.handleChange}
-                />
-                <label className="fl-input__form-duration" htmlFor="extraLong">
-                  <h3 className="fl-input__form-label-title">
-                    Extra-long Haul
-                  </h3>
-                  <p className="fl-input__form-label-info">
-                    Up to 16:40h or 14500km / 9000mi
-                  </p>
-                  <p className="fl-input__form-label-example">
-                    e.g. London - Perth
-                  </p>
-                </label>
-
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="ultraLong"
-                  name="flightDuration"
-                  value="ultraLong"
-                  onChange={this.handleChange}
-                />
-                <label className="fl-input__form-duration" htmlFor="ultraLong">
-                  <h3 className="fl-input__form-label-title">Ulta-long Haul</h3>
-                  <p className="fl-input__form-label-info">
-                    Up to 24:00h or 18400km / 11370mi
-                  </p>
-                  <p className="fl-input__form-label-example">
-                    e.g. London - Auckland
-                  </p>
-                </label>
-              </div>
-            </div>
-
-            <div className="fl-input__break"></div>
-
-            <div className="fl-input__form-question">
-              <label className="fl-input__form-label">
-                <span className="span">{'>>'} </span> What class did you fly in?
-              </label>
-
-              <div className="fl-input__form-class-box">
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="economy"
-                  name="flightClass"
-                  value="Economy"
-                  onChange={this.handleClassChange}
-                />
-                <label className="fl-input__form-class" htmlFor="economy">
-                  <h3 className="fl-input__form-label-title-class">
-                    🌱 Economy
-                    <br /> Class
-                  </h3>
-                </label>
-
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="premiumEconomy"
-                  name="flightClass"
-                  value="PremiumEconomy"
-                  onChange={this.handleClassChange}
-                />
-                <label
-                  className="fl-input__form-class"
-                  htmlFor="premiumEconomy"
-                >
-                  <h3 className="fl-input__form-label-title-class">
-                    💸 Premium
-                    <br /> Economy{' '}
-                  </h3>
-                </label>
-              </div>
-
-              <div className="fl-input__form-class-box">
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="Business"
-                  name="flightClass"
-                  value="Business"
-                  onChange={this.handleClassChange}
-                />
-                <label className="fl-input__form-class" htmlFor="Business">
-                  <h3 className="fl-input__form-label-title-class">
-                    💰 Business
-                    <br /> Class
-                  </h3>
-                </label>
-
-                <input
-                  className="fl-input__form-radio"
-                  type="radio"
-                  id="first"
-                  name="flightClass"
-                  value="First"
-                  onChange={this.handleClassChange}
-                />
-                <label className="fl-input__form-class" htmlFor="first">
-                  <h3 className="fl-input__form-label-title-class">
-                    🧐 First <br />
-                    Class
-                  </h3>
-                </label>
-              </div>
-            </div>
-          </form>
-        </div>
+        <FlightsInput
+          handleChange={this.handleChange}
+          handleClassChange={this.handleClassChange}
+        />
 
         {/* //! output section (right side) starts here */}
-        <div className="fl-output">
-          {this.state.chartData ? (
-            <div className="fl-output__content">
-              <div className="fl-output__content-title">
-                Kilograms of CO2 released by <br /> your flight in{' '}
-                {this.state.userFlightClass} Class. <br />
-                Comparison with other classes and <br /> to an equivalent
-                distance covered by train.
-              </div>
-              {this.flightsChart()}
-              <div className="fl-output__content-text">
-                <div className="fl-output__info-icon">i</div>
-                Your flight released approx.{' '}
-                <span className="span--bold">
-                  {' '}
-                  {Number(this.state.totalFlightCo2).toFixed(0)} kilograms of
-                  CO2{' '}
-                </span>{' '}
-                into the atmosphere. Covering the same distance by train would
-                have generated{' '}
-                <span className="span--bold">
-                  {' '}
-                  {Number(
-                    (this.state.userFlightDistance * 0.021).toFixed(0)
-                  )}{' '}
-                  kilograms of CO2
-                </span>{' '}
-                instead.
-              </div>
-              <div className="fl-output__content-button-box">
-                {/* <div className="fl-output__content-button">
-                  Save estimate in dashboard
-                  <span className="span"> {'>>'} </span>
-                </div> */}
-                <Link
-                  onClick={this.saveFlCo2}
-                  className="fl-output__link"
-                  to="/estimate/vehicles"
-                >
-                  <div className="fl-output__content-button fl-output__content-button--next">
-                    Go to Next Step <span className="span"> {'>>'} </span>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          ) : (
-            <div className="fl-output__intro">
-              <h3 className="fl-output__intro-title">
-                Flying often represents a
-                <br />
-                signifcant part of people's footprint. <br />
-                You can use the map below as a guide
-                <br />
-                to estimate yours.
-              </h3>
-              <img
-                className="fl-output__intro-image"
-                alt="home electricity consumption and network"
-                src={flightsMap}
-              />
-              <img
-                className="fl-output__intro-logo"
-                alt="co2 logo"
-                src={co2Logo}
-              />
-            </div>
-          )}
-        </div>
+
+        <FlightsOutput
+          chartData={this.state.chartData}
+          userFlightClass={this.state.userFlightClass}
+          totalFlightCo2={this.state.totalFlightCo2}
+          userFlightDistance={this.state.userFlightDistance}
+          saveFlCo2={this.state.saveFlCo2}
+          flightsChart={this.flightsChart}
+        />
       </div>
     );
   }
