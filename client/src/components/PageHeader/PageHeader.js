13d09a7de7f0fc09087577f652acc0e6c3307c00
Felipe Franco <felipefranco8610@gmail.com>
2022-02-19 18:44:07 +0000

    Update Header component to make it responsive

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -1,45 +1,90 @@
-import React from 'react';
-import './PageHeader.scss';
-import { Link, NavLink } from 'react-router-dom';
-import greenWiseLogo from '../../assets/images/images/greenwise-logo.svg';
+import React, { Component } from "react";
+import "./PageHeader.scss";
+import { Link, NavLink } from "react-router-dom";
+import greenWiseLogo from "../../assets/images/images/greenwise-logo.svg";
+import menuIcon from "../../assets/images/icons/menu-icon.png";
 
-export default function PageHeader() {
-  return (
-    <header className="header">
-      <div className="header__main-container">
-        <Link className="header__logo-container" to="/">
-          <img
-            className="header__logo"
-            alt="green-wise logo"
-            src={greenWiseLogo}
-          />
-        </Link>
-        <div className="header__buttons-container">
-          <NavLink className="header__link" to="/estimate">
-            <h3 className="header__button-text ">
-              1. Estimate <span className="span"> &gt;&gt; </span>{' '}
-            </h3>
-          </NavLink>
-          <div className="header__button-spacer"></div>
-          <NavLink className="header__link" to="/reduce">
-            <h3 className="header__button-text">
-              2. Reduce <span className="span"> &gt;&gt; </span>{' '}
-            </h3>
-          </NavLink>
-          <div className="header__button-spacer"></div>
-          <NavLink className="header__link" to="/compensate">
-            <h3 className="header__button-text">
-              3. Compensate <span className="span"> &gt;&gt; </span>
-            </h3>
-          </NavLink>
-          <div className="header__button-spacer"></div>
-          <NavLink className="header__link" to="/my-account">
-            <h3 className="header__button-text">
-              My Account <span className="span"> &gt;&gt; </span>
-            </h3>
-          </NavLink>
+export default class PageHeader extends Component {
+  state = {
+    menuVisible: false,
+  };
+
+  handleMenu = () => {
+    this.setState({ menuVisible: !this.state.menuVisible });
+  };
+
+  render() {
+    return (
+      <header className="header">
+        <div className="header__main-container">
+          <Link className="header__logo-container" to="/">
+            <img
+              className="header__logo"
+              alt="green-wise logo"
+              src={greenWiseLogo}
+            />
+          </Link>
+          <div
+            onClick={this.handleMenu}
+            className="header__menu-icon-container"
+          >
+            <img className="header__menu-icon" alt="menu icon" src={menuIcon} />
+          </div>
+
+          <div
+            className={
+              this.state.menuVisible
+                ? "header__buttons-container"
+                : "header__buttons-container--hidden"
+            }
+          >
+            <div
+              className={
+                this.state.menuVisible
+                  ? "header__buttons-subcontainer"
+                  : "header__buttons-subcontainer--hidden"
+              }
+            >
+              <NavLink className="header__link" to="/estimate">
+                <h3 className="header__button-text ">
+                  <span>1. Estimate</span>{" "}
+                  <span className="span"> &gt;&gt; </span>{" "}
+                </h3>
+              </NavLink>
+              <div className="header__button-spacer"></div>
+              <NavLink className="header__link" to="/reduce">
+                <h3 className="header__button-text">
+                  <span>2. Reduce</span>{" "}
+                  <span className="span"> &gt;&gt; </span>{" "}
+                </h3>
+              </NavLink>
+            </div>
+            <div className="header__button-spacer"></div>
+            <div
+              className={
+                this.state.menuVisible
+                  ? "header__buttons-subcontainer"
+                  : "header__buttons-subcontainer--hidden"
+              }
+            >
+              {" "}
+              <NavLink className="header__link" to="/compensate">
+                <h3 className="header__button-text">
+                  <span>3. Compensate</span>{" "}
+                  <span className="span"> &gt;&gt; </span>
+                </h3>
+              </NavLink>
+              <div className="header__button-spacer"></div>
+              <NavLink className="header__link" to="/my-account">
+                <h3 className="header__button-text">
+                  <span>My Account</span>{" "}
+                  <span className="span"> &gt;&gt; </span>
+                </h3>
+              </NavLink>
+            </div>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  }
 }
