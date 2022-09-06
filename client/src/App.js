e5f1c8bd170269910984b62a852fd4c155487310
Felipe Franco <felipefranco8610@gmail.com>
2022-09-06 20:34:20 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Estimate from './pages/estimate';
-import Electricity from './pages/estimate/Electricity';
-import Flights from './pages/estimate/Flights';
-import Vehicles from './pages/estimate/Vehicles';
-import Home from './pages/home';
-import Compensate from './pages/compensate';
-import Profile from './pages/profile';
-import Reduce from './pages/reduce';
-import './styles/App.scss';
-import Login from './pages/profile/Login';
-import Register from './pages/profile/Register';
-import Help from './pages/profile/Help';
-import AboutInterface from './components/AboutInterface/AboutInterface';
-import IntroInterface from './components/IntroInterface/IntroInterface';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Estimate from "./pages/estimate";
+import Electricity from "./pages/estimate/Electricity";
+import Flights from "./pages/estimate/Flights";
+import Vehicles from "./pages/estimate/Vehicles";
+import Home from "./pages/home";
+import Compensate from "./pages/compensate";
+import Profile from "./pages/profile";
+import Reduce from "./pages/reduce";
+import "./styles/App.scss";
+import Login from "./pages/profile/Login";
+import Register from "./pages/profile/Register";
+import Help from "./pages/profile/Help";
+import AboutInterface from "./components/AboutInterface/AboutInterface";
+import IntroInterface from "./components/IntroInterface/IntroInterface";
 
 export default class App extends Component {
   render() {
@@ -22,18 +22,18 @@ export default class App extends Component {
         <BrowserRouter>
           <div className="app">
             <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/estimate" exact component={Estimate} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/estimate" component={Estimate} />
               <Route
-                path="/estimate/electricity"
                 exact
+                path="/estimate/electricity"
                 component={Electricity}
               />
-              <Route path="/estimate/flights" exact component={Flights} />
-              <Route path="/estimate/vehicles" exact component={Vehicles} />
-              <Route path="/reduce" component={Reduce} />
-              <Route path="/compensate" component={Compensate} />
-              <Route path="/my-account" component={Profile} />
+              <Route exact path="/estimate/flights" component={Flights} />
+              <Route exact path="/estimate/vehicles" component={Vehicles} />
+              <Route exact path="/reduce" component={Reduce} />
+              <Route exact path="/compensate" component={Compensate} />
+              <Route exact path="/my-account" component={Profile} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/help" component={Help} />
